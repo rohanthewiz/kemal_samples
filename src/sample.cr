90ecebb1d034d6c3ef9ea6acebe832c014d266dc
Rohan Allison <rohanthewiz_at_gmail.com>
2016-11-26 22:18:58 -0600

    Add JSON api

--- src/sample.cr
+++ src/sample.cr
@@ -6,6 +6,12 @@ module Sample
     "Hello from application home"
   end
   
+  get "/myapi" do |ctx|
+  	ctx.response.content_type = "application/json"
+  	{name: "Serdar", age: 27}.to_json
+  end
+  
+  # Optional custom 404 handler
   error 404 do
     "Dude, I couldn't find that page!"
   end
